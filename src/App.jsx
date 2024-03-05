051fd44d5bb7eaa9325be525c60b318292054908
정찬영 <q135794268020@gmail.com>
2024-03-06 00:44:09 +0900

    state로 상태관리

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,31 @@
 import './App.css';
-
+import { useState } from 'react';
 import React from 'react';
-import Button from './components/Button';
-import Header from './components/Header';
 
 function App() {
-  const buttonProps = {
-    text: '메일',
-    color: 'red',
+  // 배열의 구조분해할당
+  const [count, setCount] = useState(0);
+  const [light, setLight] = useState('OFF');
+  // 리액트는 state값이 변화하였을 때 렌더링 되기 떄문에 가변적인 값을 관리할 떄 일반 변수가 아닌 state를 이용해서 처리한다.
+
+  const UpCount = () => {
+    setCount(count + 1);
   };
+
+  const chageLight = () => {
+    setLight(light === 'OFF' ? 'ON' : 'OFF');
+  };
+
   return (
     <>
-      <Button {...buttonProps} />
-      <Button text={'카페'} />
-      <Button text={'고양이'} />
-      <Button color={'red'}>
-        <div>자식요소</div>
-        <Header />
-      </Button>
+      <div>
+        <h1>{light}</h1>
+      </div>
+      <button onClick={chageLight}>{light === 'OFF' ? '켜기' : '끄기'}</button>
+      <div>
+        <h1>{count}</h1>
+        <button onClick={UpCount}>+</button>
+      </div>
     </>
   );
 }
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
-// 객체의 구조분해할당 문법을 이용하여 가져오려는 값을 바로 꺼내올 수 있다.
-// props는 부모에서 자식으로만 전달 가능
 const Button = ({ text, color, children }) => {
+  const onClickButton = (e) => {
+    console.log(e);
+    console.log(text);
+  };
   return (
-    <button style={{ color: color }}>
+    <button
+      //   onMouseEnter={onClickButton}
+      onClick={onClickButton}
+      style={{ color: color }}
+    >
       {text} - {color}
       {children}
     </button>
