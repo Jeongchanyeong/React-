74274f61c5fdc3241397443c6a2e60e081dac671
정찬영 <q135794268020@gmail.com>
2024-03-06 12:24:41 +0900

    git test

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,8 @@
 import './App.css';
-import { useEffect, useState } from 'react';
 import React from 'react';
-import Register from './components/Register';
-import Hook from './components/Hook';
-
-// 자신의 state가 변하지 않아도 부모로부터 받은 pops가 변경되면 리렌더링 된다.
-// 랜더링을 최소화 하기 위해 각각의 state들은 각각의 컴포넌트 내부에 배치하는 것이 좋다.
-
-/** 리렌더링 조건!
-  1. state가 변경되었을 떄
-  2. props가 변경되었을 때
-  3. 부모가 리렌더링이 발생할 떄
- */
 
 function App() {
-  return (
-    <>
-      <Hook />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
--- src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Footer = () => {
-  return <div>Footer입니다.</div>;
-};
-
-export default Footer;
